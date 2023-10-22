0a0c85299022998de32d568013d2159d072763a3
julio gomes <juliogomesj09@gmail.com>
2023-10-21 22:26:33 -0300

    Initial commit

---
+++ src/test/java/dio/apipub/ApiPubApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.apipub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
